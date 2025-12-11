d6e4d22424990204d74bf0c979ab329c6ac8c22d
SamuelPCS <svogel@pcsknox.com>
2025-12-11 16:09:38 -0500

    refactored webrequest to be in Golang

--- 12-12-25/ListUsers.go
+++ 12-12-25/ListUsers.go
@@ -1,8 +1,37 @@
 
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    // The URL you want to request
+    url := "https://httpbin.org/get"
+
+    // Perform a simple GET
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Println("Request error:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    // Check HTTP status code
+    if resp.StatusCode != http.StatusOK {
+        fmt.Printf("Unexpected status: %s\n", resp.Status)
+        return
+    }
+
+    // Read and print the response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Read error:", err)
+        return
+    }
+
+    fmt.Println("Response:")
+    fmt.Println(string(body))
 }
